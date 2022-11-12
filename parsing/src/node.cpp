e01dd0fae7fb86914b80df9b78a2d68bc9552be7
Akanksha Kumar <50279007+akankshakumar11@users.noreply.github.com>
2022-11-12 00:00:35 -0600

    Update node.cpp

--- parsing/src/node.cpp
+++ parsing/src/node.cpp
@@ -19,6 +19,20 @@ namespace Parser {
     }
 
     string Node::getData() {
-        return data_;
+        std::string s = data_;
+        std::string delimiter = ",";
+        std::map<std::string, int> m;
+
+        size_t pos = 0;
+        std::string token;
+        while ((pos = s.find(delimiter)) != std::string::npos) {
+            token = s.substr(0, pos);
+            std::cout << token << std::endl;
+//             if (!(m.count(token))) {
+//                 m.insert(std::pair<std::string,int>('a',100));
+//             }
+            s.erase(0, pos + delimiter.length());
+        }
+            return data_;
     }
 }
