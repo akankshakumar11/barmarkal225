7a0050385631065b7e0ffac10282a8cff3be4ce9
ananyab3 <89149281+ananyab3@users.noreply.github.com>
2022-12-11 13:59:24 -0500

    added BFS files

---
+++ BFS.cpp
@@ -0,0 +1,109 @@
+#include "BFS.h"
+#include <queue>
+#include <vector>
+
+
+BFS::BFS(std::vector<Vertex> vect, Vertex start) {
+    adj_verts_ = std::map<Vertex, std::vector<Vertex>>();
+    is_visited_ = std::map<Vertex, bool>();
+    bfs_queue_.push(start);
+
+    for (Vertex v : vect) {
+        verts_.push_back(v);
+    }
+
+    for (Vertex v2 : verts_) {
+        is_visited_[v2] = false;
+    }
+
+    is_visited_[start] = true;
+}
+
+
+BFS::BFS(std::vector<Vertex> vect, std::vector<Edge> edge, Vertex start) {
+    adj_verts_ = std::map<Vertex, std::vector<Vertex>>();
+    is_visited_ = std::map<Vertex, bool>();
+    bfs_queue_.push(start);
+
+    for (Vertex v : vect) {
+        verts_.push_back(v);
+    }
+
+    for (Edge e : edge) {
+        adj_verts_[e.source].push_back(e.dest);
+        adj_verts_[e.dest].push_back(e.source);
+    }
+
+    for (Vertex v2 : verts_) {
+        is_visited_[v2] = false;
+    }
+
+    is_visited_[start] = true;
+}
+
+
+BFS::~BFS() {
+    bfs_queue_.empty();
+    is_visited_.clear();
+    adj_verts_.clear();
+    verts_.clear();
+}
+
+
+void BFS::addEdge(Vertex vert1, Vertex vert2) {
+    adj_verts_[vert1].push_back(vert2);
+    adj_verts_[vert2].push_back(vert1);
+}
+
+
+void BFS::addEdge(Edge edge) {
+    adj_verts_[edge.source].push_back(edge.dest);
+    adj_verts_[edge.dest].push_back(edge.source);
+}
+
+
+void BFS::pop() {
+    bfs_queue_.pop();
+}
+
+
+Vertex BFS::front(){
+    return bfs_queue_.front();
+}
+
+
+void BFS::startBFS() {
+    std::list<int>::iterator iter;
+
+    while (!bfs_queue_.empty()) {
+        Vertex v = bfs_queue_.front();
+        bfs_queue_.pop();
+
+        for (Vertex iter : adj_verts_[v]) {
+            if (is_visited_[iter] == false) {
+                is_visited_[iter] = true;
+                bfs_queue_.push(iter);
+            }
+        }
+    }
+}
+
+
+Vertex BFS::slowBFS() {
+    std::list<int>::iterator i;
+
+    while (!bfs_queue_.empty()) {
+        Vertex v = bfs_queue_.front();
+        for (Vertex iter : adj_verts_[v]) {
+            if (is_visited_[iter] == false) {
+                bfs_queue_.push(iter);
+                is_visited_[iter] = true;
+                return iter;
+            }
+        }
+
+        bfs_queue_.pop();
+    }
+
+    return NULL;
+}
---
+++ BFS.h
@@ -0,0 +1,79 @@
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include "graph.h"
+#include "edge.h"
+
+class BFS
+{
+public:
+    /**
+     * Creates BFS object
+     * @param vect - 2-dimensional vector of vertices
+     * @param start - starting vertex
+    */
+    BFS(std::vector<Vertex> vect, Vertex start);
+
+    /**
+     * Creates BFS object
+     * @param vect - 2-dimensional vector of vertices
+     * @param edges - 2-dimensional vector of edges
+     * @param start - starting vertex
+    */
+    BFS(std::vector<Vertex> vect, std::vector<Edge> edge, Vertex start);
+
+    /**
+     * Destroys BFS object
+    */
+    ~BFS();
+
+    /**
+     * Adds edge between two vertices
+     * @param vert1 - starting vertex
+     * @param vert2 - ending vertex
+    */
+    void addEdge(Vertex vert1, Vertex vert2);
+
+    /**
+     * Adds edge
+     * @param edge - edge to add
+    */
+    void addEdge(Edge edge);
+
+    /**
+     * Pops vertex at the front of the queue
+    */
+    void pop();
+
+     /**
+     * Returns vertex at the front of the queue
+     * @return vertex at the front of the queue
+    */
+    Vertex front();
+
+    /**
+     * Starts the BFS traversal
+    */
+    void startBFS();
+
+    /**
+     * Conducts BFS slowly
+     * @return next vertex in the  (technically null)
+    */
+    Vertex slowBFS();
+
+private:
+    /* Vector of vertices */
+    std::vector<Vertex> verts_;
+
+    /* Map whose pairs are of form (vertex, vector of adjacent vertices) */
+    std::map<Vertex, std::vector<Vertex>> adj_verts_;
+
+    /* Queue of vertices to visit during the BFS traversal */
+    std::queue<Vertex> bfs_queue_;
+
+    /* Map whose pairs are of form (vertex, bool indicating whether or not the vertex has been visited) */
+    std::map<Vertex, bool> is_visited_;
+};
